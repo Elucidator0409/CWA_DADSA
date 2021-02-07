7fb4165eecffb9135fa56362e85b162fcfcb9e0e
Elucidator0409 <tranquockhanhdhsp99@gmail.com>
2021-02-08 00:18:40 +0700

    new upd

--- Order.py
+++ Order.py
@@ -2,5 +2,8 @@ class Order:
     def __init__(self, name,itemlist , need):
         self.name = name
         self.itemlist = itemlist
+        self.tempItemList = itemlist
+        
+        self.quantityList  = []
         self.need = need
         #self.print = print("Order of {0} is : {1} {2}".format(name,itemlist,need))
--- cwA.py
+++ cwA.py
@@ -28,15 +28,15 @@ def toDO():
     itemList, costList, storeName = read.readItemList()
     nameList = read.readNameList()
     productList = []
-    
+    quantityList = []
     buyList = []
-    
+   
     
     
     for i in range(0, 27):
         tempProduct = Product(itemList[i], costList[i])
         productList.append(tempProduct)
-    tempProductList = productList
+  
     
    
     #for y in tempProductList:
@@ -45,18 +45,18 @@ def toDO():
 
     
     testName=["1A"]   
-    
+    '''
     for a in testName:
         tempOrder = order.searchNode(a)
         tempNeed = tempOrder.need
-        tempItem = tempOrder.itemlist
+        tempOrder.tempItemList1 = tempOrder.itemlist
         
         for b in tempNeed:
             
                 if b > 0:
                     for c in storeName:
-                        itemCheck = tempItem[0]
-                        
+                        itemCheck = tempOrder.tempItemList1[0]
+                        print(itemCheck)
                         tempList = store.searchNode(c)
                         tempList.sortStore()
                     
@@ -64,15 +64,70 @@ def toDO():
                         if result == True:
                             
                             buyList.append(itemCheck)
-                            if len(tempItem)>=1:
-                                tempItem.pop(0)
+                            if len(tempOrder.tempItemList1)>=1:
+                                tempOrder.tempItemList1.pop(0)
                             break
                         
-                elif len(tempItem)>=1 : 
-                    tempItem.pop(0)
-        
-    for z in buyList:
-        print(z)
+                elif len(tempOrder.tempItemList1)>=1 : 
+                    tempOrder.tempItemList1.pop(0)
+                    '''
+    bought = []
+    buyInDay = []
+    tempBuyInDay = []
+    needBuy = []
+    quantityList = []
+    daysCount = 0
+    canDeli = []
+    while is_done:  #MAIN ALGO
+       
+
+        for a in testName:
+            tempOrder = order.searchNode(a)
+            tempNeed = tempOrder.need
+            
+            for i in storeName: 
+                tempList1 = store.searchNode(i)
+                tempList1.sortStore()
+      
+                for b in tempNeed:
+                    if b>0 :
+                        
+                        
+                    
+                        itemCheck = tempOrder.tempItemList1[0]
+                        check = tempList1.search(itemCheck)
+                        if check == True:
+                            tempOrder.tempItemList.remove(check)
+                            buyInDay.append(check) #c
+                            quantityList.append(b)
+                    if daysCount == 2:    
+                        if not tempOrder.tempItemList :
+                    
+                            itemDeli = bought
+                            bought = []
+                            daysCount = 0
+                        else:         
+                            needBuy = needBuy + bought
+                            bought = buyInDay
+                            buyInDay = []  
+                    bought = buyInDay  + bought  #a b c 
+                    tempBuyInDay = buyInDay# C
+                    
+                    daysCount = daysCount + 1   
+                    
+                
+                    
+                   
+                    
+                        
+                               
+               
+
+                    
+
+       
+                
+    
     
                
                         
